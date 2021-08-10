e04bb4f2481d20892e1b03bdc6e73e4ed84ab581
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-08-10 11:47:27 -0300

    Update Sprites.py

--- Sprites/Sprites.py
+++ Sprites/Sprites.py
@@ -3,12 +3,9 @@ from pygame.locals import *
 from sys import exit
 
 pygame.init()
-
 largura = 640
 altura = 480
-
 PRETO = (0,0,0)
-
 tela = pygame.display.set_mode((largura, altura))
 pygame.display.set_caption('Sprites')
 
@@ -29,12 +26,9 @@ class Sapo(pygame.sprite.Sprite):
         self.atual = 0
         self.image = self.sprites[self.atual]
         self.image = pygame.transform.scale(self.image, (128*3, 64*3))
-
         self.rect = self.image.get_rect()
         self.rect.topleft = 100, 100
-
         self.animar = False
-
     def atacar(self):
         self.animar = True
     def update(self):
@@ -45,15 +39,10 @@ class Sapo(pygame.sprite.Sprite):
                 self.animar = False
             self.image = self.sprites[int(self.atual)]
             self.image = pygame.transform.scale(self.image, (128*3, 64*3))
-
-
-
 todas_as_sprites = pygame.sprite.Group()
 sapo = Sapo()
 todas_as_sprites.add(sapo)
-
 relogio = pygame.time.Clock()
-
 while True:
     relogio.tick(30)
     tela.fill(PRETO)
@@ -63,7 +52,6 @@ while True:
             exit()
         if event.type == KEYDOWN:
             sapo.atacar()
-    
     todas_as_sprites.draw(tela)
     todas_as_sprites.update()
     pygame.display.flip()
