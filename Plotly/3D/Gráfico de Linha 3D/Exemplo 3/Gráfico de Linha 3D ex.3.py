26e067d003fd6f9130f31578d9312d9fceb65c64
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-09-13 13:13:23 -0300

    Update Gráfico de Linha 3D ex.3.py

--- Plotly/3D/Gráfico de Linha 3D/Exemplo 3/Gráfico de Linha 3D ex.3.py
+++ Plotly/3D/Gráfico de Linha 3D/Exemplo 3/Gráfico de Linha 3D ex.3.py
@@ -3,7 +3,8 @@ import plotly.express as px # Biblioteca usada para criar gráficos
 df = px.data.tips() # DataFrame com conjunto de dados
 fig = px.line_3d(df, x = 'time',
                      y = 'day',
-                     z = 'sex') # Cria o objeto com os dados de x e y.
+                     z = 'sex', 
+                     color = 'time') # Cria o objeto com os dados de x e y.
 fig.show() # Plota o gráfico
 
 # plotly.express pode criar toda a fígura de uma só vez. Ele usa a graph_objects internamente
