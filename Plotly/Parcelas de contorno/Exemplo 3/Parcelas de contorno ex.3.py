458c5986e5a40b1848edd199a66e9e96af6a4c52
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-09-13 11:21:28 -0300

    Update Parcelas de contorno ex.3.py

--- Plotly/Parcelas de contorno/Exemplo 3/Parcelas de contorno ex.3.py
+++ Plotly/Parcelas de contorno/Exemplo 3/Parcelas de contorno ex.3.py
@@ -5,10 +5,11 @@ feature_x = np.arange(0, 50, 2) # Valores do eixo X
 feature_y = np.arange(0, 50, 3) # Valores do eixo Y
 [X, Y] = np.meshgrid(feature_x, feature_y) # Cria a grade 2D
 Z = np.cos(X / 2) + np.sin(Y / 4)
+# Cria o objeto com os dados de x e y.
 fig = go.Figure(data =
      go.Contour(x = feature_x, y = feature_y, z = Z,
                colorscale='rainbow'
-               )) # Cria o objeto com os dados de x e y.
+               )) 
 fig.show() # Plota o gráfico
 
 # plotly.express pode criar toda a fígura de uma só vez. Ele usa a graph_objects internamente
