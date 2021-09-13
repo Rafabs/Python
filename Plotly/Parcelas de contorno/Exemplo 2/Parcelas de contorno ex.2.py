ec2e2e0b2b4599c2e7243c801e558c0fe8ac5ea3
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-09-13 11:19:21 -0300

    Update Parcelas de contorno ex.2.py

--- Plotly/Parcelas de contorno/Exemplo 2/Parcelas de contorno ex.2.py
+++ Plotly/Parcelas de contorno/Exemplo 2/Parcelas de contorno ex.2.py
@@ -6,7 +6,7 @@ data = [[1,2,3,4,5],
         [7,8,9,6,4],
         [3,7,2,4,2]]
 
-fig = go.Figure(data = go.Contour(z = data)) # Cria o objeto com os dados de x e y automaticamente por plotly.
+fig = go.Figure(data = go.Contour(z = data)) # Cria o objeto com os dados de x e y.
 fig.show() # Plota o gráfico
 
 # plotly.express pode criar toda a fígura de uma só vez. Ele usa a graph_objects internamente
