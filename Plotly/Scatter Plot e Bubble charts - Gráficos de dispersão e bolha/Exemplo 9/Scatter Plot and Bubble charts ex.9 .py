c57ce87870a40db2aab759154ebc92f4d1df3ef9
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-09-13 10:47:24 -0300

    Update Scatter Plot and Bubble charts ex.9 .py

--- Plotly/Scatter Plot e Bubble charts - Gráficos de dispersão e bolha/Exemplo 9/Scatter Plot and Bubble charts ex.9 .py
+++ Plotly/Scatter Plot e Bubble charts - Gráficos de dispersão e bolha/Exemplo 9/Scatter Plot and Bubble charts ex.9 .py
@@ -4,6 +4,7 @@ import numpy as np
 np.random.seed(42)
 random_x = np.random.randint(1, 101, 100)
 random_y = np.random.randint(1, 101, 100)
+# Cria o objeto com os dados de x e y
 fig = px.Figure(data=[px.Scatter(
     x = random_x,
     y = random_y,
@@ -13,7 +14,7 @@ fig = px.Figure(data=[px.Scatter(
        size = [10, 30, 50, 80],
        showscale=True
      )
-)]) # Cria o objeto com os dados de x e y
+)]) 
 fig.show() # Plota o gráfico
 
 # plotly.express pode criar toda a fígura de uma só vez. Ele usa a graph_objects internamente
