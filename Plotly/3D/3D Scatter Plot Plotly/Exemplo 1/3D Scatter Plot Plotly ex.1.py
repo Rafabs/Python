df6b19772d22f3f85b139e4628b5dd4b5e3728c1
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-09-13 13:16:30 -0300

    Update 3D Scatter Plot Plotly ex.1.py

--- Plotly/3D/3D Scatter Plot Plotly/Exemplo 1/3D Scatter Plot Plotly ex.1.py
+++ Plotly/3D/3D Scatter Plot Plotly/Exemplo 1/3D Scatter Plot Plotly ex.1.py
@@ -4,7 +4,7 @@ df = px.data.iris() # DataFrame com conjunto de dados iris
 fig = px.scatter_3d(df, x = 'sepal_width',
                     y = 'sepal_length',
                     z = 'petal_width',
-                    color = 'species')  # Cria o objeto com os dados de x e y automaticamente por plotly.
+                    color = 'species')  # Cria o objeto com os dados de x e y.
 fig.show() # Plota o gráfico
 
 # plotly.express pode criar toda a fígura de uma só vez. Ele usa a graph_objects internamente
