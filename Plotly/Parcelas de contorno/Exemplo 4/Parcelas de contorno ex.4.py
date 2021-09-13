8d4b0c3056c7b3fd69a8db06c8cf8cec6a1dea7c
Rafael Barbosa <76046909+Rafabs@users.noreply.github.com>
2021-09-13 11:23:43 -0300

    Update Parcelas de contorno ex.4.py

--- Plotly/Parcelas de contorno/Exemplo 4/Parcelas de contorno ex.4.py
+++ Plotly/Parcelas de contorno/Exemplo 4/Parcelas de contorno ex.4.py
@@ -5,7 +5,7 @@ feature_x = np.arange(0, 50, 2) # Valores do eixo X
 feature_y = np.arange(0, 50, 3) # Valores do eixo Y
 [X, Y] = np.meshgrid(feature_x, feature_y) # Cria a grade 2D
 Z = np.cos(X / 2) + np.sin(Y / 4)
-fig = go.Figure(data=go.Contour( # Cria o objeto com os dados de x e y automaticamente por plotly.
+fig = go.Figure(data=go.Contour( # Cria o objeto com os dados de x e y.
     x=feature_x, y=feature_y, z=Z,
     contours=dict(
         coloring='lines',
